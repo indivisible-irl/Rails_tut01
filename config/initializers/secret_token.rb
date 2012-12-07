e0868f527f09e8f5497249d154b9cabcf039d173
indivisible-irl <mbspare@gmail.com>
2012-12-07 15:43:29 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tut01::Application.config.secret_token = 'c6a56917857f33654aab064ddafd473c6d1ec39db143fe48fe5650e73f7fd1c78740361b0f30de28449aae05917a7089735b155f25ee3223cd241c2226b40241'
